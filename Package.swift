6e681076fe4b21804befbafe2c3998b1fb315c42
Nazar Ilamanov <nazar.ilamanov@gmail.com>
2021-04-24 23:18:11 -0400

    Add platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPieChart",
+    platforms: [
+        .iOS(.v13), .macOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
